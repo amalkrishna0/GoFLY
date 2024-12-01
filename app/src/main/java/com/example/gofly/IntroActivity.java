95d45a7a0fa9f113350533b639c22c87761b9c1e
amalkrishna0 <amalkrishna528@gmail.com>
2024-12-01 09:53:06 +0530

    Created Intro Activity , also updated with firebase sdk to the project

---
+++ app/src/main/java/com/example/gofly/IntroActivity.java
@@ -0,0 +1,27 @@
+package com.example.gofly;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+
+    Button btnStart, btnHave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        btnStart = findViewById(R.id.startBtn);
+        btnHave = findViewById(R.id.haveBtn);
+    }
+}
